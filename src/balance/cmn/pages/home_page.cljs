e27f7cc5824407e70248636ad1971fb4bbe3822f
Denis Krivosheev <denis.takeda@gmail.com>
2017-05-16 16:25:22 +0500

    Implement task item

--- src/balance/cmn/pages/home_page.cljs
+++ src/balance/cmn/pages/home_page.cljs
@@ -3,10 +3,14 @@
             [reagent.core :as r]
             [re-frame.core :refer [subscribe]]))
 
-(def styles {:page      {:margin-top     64
-                         :flex-grow      1
-                         :flex-direction "row"}
-             :task-list {:flex-grow 1}})
+(def styles {:page             {:margin-top     64
+                                :flex-grow      1
+                                :flex-direction "row"}
+             :task-list        {:flex-grow 1}
+             :task-item        {:padding 10}
+             :task-caption     {:font-size 18
+                                :color "gray"}
+             :task-description {:font-size 12}})
 
 (defn render-item [item]
   (r/as-element [task-item (-> item .-item .-key)]))
@@ -21,6 +25,8 @@
 
 (defn task-item [task-id]
   (fn []
-    (let [task (subscribe [:get-task task-id])]
-      [rn/view
-       [rn/text (str @task)]])))
+    (let [task (subscribe [:get-task task-id])
+          {:keys [caption description]} @task]
+      [rn/view {:style (:task-item styles)}
+       [rn/text {:style (:task-caption styles)} caption]
+       [rn/text {:style (:task-description styles)} description]])))
